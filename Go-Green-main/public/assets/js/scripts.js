87c5daad3296acb09fafaa89edb08cb66f279a33
Anisha <nirubollapragada@yahoo.com>
2023-08-09 00:19:22 -0700

    gg

--- Go-Green-main/public/assets/js/scripts.js
+++ Go-Green-main/public/assets/js/scripts.js
@@ -1,3 +1,82 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+window.addEventListener("scroll", function () {
+  var fadeRows = document.querySelectorAll(".row");
+
+  for (var i = 0; i < fadeRows.length; i++) {
+    var rowPosition = fadeRows[i].getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (rowPosition - windowHeight <= 0) {
+      fadeRows[i].classList.add("visible");
+    } else {
+      fadeRows[i].classList.remove("visible");
+    }
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  let countElement = document.getElementById("counter1");
+  let countElement2 = document.getElementById("counter2");
+  let countElement3 = document.getElementById("counter3");
+  let count = 0;
+  let count2 = 114800;
+  let count3 = 0;
+  let counts;
+  let counts2;
+  let counts3;
+  let seen1 = false;
+
+  function updateCounter() {
+    countElement.innerHTML = ++count;
+    if (count === 193) {
+      clearInterval(counts);
+      countElement.innerHTML = 193 + " ";
+    }
+  }
+
+  function updateCounter2() {
+    countElement2.innerHTML = ++count2;
+    if (count2 === 115000) {
+      clearInterval(counts2);
+      countElement2.innerHTML = 115000;
+    }
+  }
+
+  function updateCounter3() {
+    countElement3.innerHTML = ++count3;
+    if (count3 === 350) {
+      clearInterval(counts3);
+      countElement3.innerHTML = 350;
+    }
+  }
+
+  function startAnimation() {
+    counts = setInterval(updateCounter, 10);
+    counts2 = setInterval(updateCounter2, 10);
+    counts3 = setInterval(updateCounter3, 10);
+  }
+
+  // Add a scroll event listener
+  window.addEventListener('scroll', () => {
+    // Define the scroll position threshold
+    const threshold = 300; // Adjust this value as needed
+
+    // Get the vertical scroll position
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (seen1 === false) {
+      // Check if the scroll position has passed the threshold
+      if (scrollPosition > threshold) {
+        startAnimation();
+        seen1 = true;
+        // Remove the scroll event listener after executing the animation
+        window.removeEventListener('scroll', arguments.callee);
+      }
+    }
+  });
+});
+
 function ready(fn) {
   if (document.readyState !== "loading") {
     fn();
@@ -79,3 +158,23 @@ checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
   else
     checkList.classList.add('visible');
 }
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Add a click event listener to the dropdown button
+  document.querySelector('.dropdown-btn').addEventListener('click', function() {
+    var dropdownContent = this.nextElementSibling;
+    dropdownContent.classList.toggle('active');
+  });
+
+  // Close the dropdown when clicking outside of it
+  document.addEventListener('click', function(event) {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    var dropdownBtn = document.querySelector('.dropdown-btn');
+
+    if (!dropdownBtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+      dropdownContent.classList.remove('active');
+    }
+  });
+});
